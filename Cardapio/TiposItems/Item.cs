4bbb212dfc1a789ac4096f3ca5da052662e55ae7
Luana <luhyxi@gmail.com>
2023-12-17 14:20:57 -0300

    New item logic and parameters implemented

--- Cardapio/TiposItems/Item.cs
+++ Cardapio/TiposItems/Item.cs
@@ -1,20 +1,38 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Cardapio.TiposItems
 {
-    public abstract class Item
+    namespace Cardapio.TiposItems
     {
-        public string Nome { get; }
-        public decimal? Preco { get; set; }
-        public string? Descricao { get; set; }
-        public double? Calorias { get; set; }
-
-        public Item(string nome, decimal preco, string? descricao, double calorias)
+        public class Item
         {
-            Nome = nome;
-            Preco = preco;
-            Descricao = descricao;
-            Calorias = calorias;
+            public string Nome { get; set; }
+            public TipoPrato Tipo { get; set; }
+            public bool Vegetariano{ get; set; }
+            public decimal Preco { get; set; }
+            public string Descricao { get; set; }
+            public double Calorias { get; set; }
+
+            public Item() { }  // Add a parameterless constructor
+
+            public Item(string nome, TipoPrato tipo, bool vegetariano, decimal preco, string descricao, double calorias)
+            {
+                Nome = nome;
+                Vegetariano = vegetariano;
+                Tipo = tipo;
+                Preco = preco;
+                Descricao = descricao;
+                Calorias = calorias;
+            }
+
+            public Item(string nome, decimal preco, string? descricao, double calorias)
+            {
+                Nome = nome;
+                Preco = preco;
+                Descricao = descricao;
+                Calorias = calorias;
+            }
         }
     }
 }
