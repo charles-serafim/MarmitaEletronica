d0202ad2a7a2bb3b5ede70b20b0ad7aa68713504
Luana <luhyxi@gmail.com>
2023-12-17 14:21:51 -0300

    Item.json added for parsing, items must be put here

--- Cardapio/Program.cs
+++ Cardapio/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Cardapio.TiposCardapios;
-<<<<<<< HEAD
-using Cardapio.TiposCardapios;
-=======
 using Cardapio.TiposItems;
 using System;
->>>>>>> 7352ef38a05aae5b49694eeb767fed5608968935
 
 namespace Cardapio
 {
@@ -12,31 +8,8 @@ namespace Cardapio
     {
         static void Main(string[] args)
         {
-            var cardapioBebidas = new CardapioBebidas("Bebidas");
-            var cardapioEntradas = new CardapioEntradas("Entradas");
-            var cardapioPratos = new CardapioPratos("Pratos Principais");
-            var cardapioSaladas = new CardapioSaladas("Saladas");
-
-            var agua = new ItemBebidas("Água", 1.20m, "Água mineral", 0, false, true);
-            var suco = new ItemBebidas("Suco de Laranja", 2.50m, "Suco natural de laranja de 500ml", 120, false, true);
-            var saladaFolhas = new ItemSaladas("Salada de Folhas", 7.50m, "Mix de folhas verdes", 50, false, true);
-            var saladaVegetal = new ItemSaladas("Salada de Vegetais", 8.00m, "Mix de vegetais frescos", 60, true, false);
-            var entrada = new ItemEntradas("Entrada", 5.00m, "Entrada saborosa", 80);
-            var pratoPrincipal = new ItemPratosPrincipais("Prato Principal", 12.00m, "Prato saboroso principal", 200);
-
-            cardapioBebidas.AdicionarItem(agua);
-            cardapioBebidas.AdicionarItem(suco);
-            cardapioSaladas.AdicionarItem(saladaFolhas);
-            cardapioSaladas.AdicionarItem(saladaVegetal);
-            cardapioEntradas.AdicionarItem(entrada);
-            cardapioPratos.AdicionarItem(pratoPrincipal);
-
-            cardapioBebidas.MostrarCardapio();
-            cardapioSaladas.MostrarCardapio();
-            cardapioEntradas.MostrarCardapio();
-            cardapioPratos.MostrarCardapio();
-
-            Console.ReadLine();
+            var ParserJson = JsonParser.Parse();
+            JsonParser.Printing(ParserJson);
         }
     }
 }
