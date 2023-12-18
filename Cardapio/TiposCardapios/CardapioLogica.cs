09e5ec80c81304c981bfe24a9b3db44c827e924e
Luana <luhyxi@gmail.com>
2023-12-18 09:28:16 -0300

    Nova ´lógica para parsing

--- Cardapio/TiposCardapios/CardapioLogica.cs
+++ Cardapio/TiposCardapios/CardapioLogica.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Generic;
+using System.Linq; // Added for OrderBy and ThenBy
 using Cardapio.TiposItems;
 
 namespace Cardapio.TiposCardapios
 {
-    public class CardapioLogica:JsonParser
+    public class CardapioLogica : JsonParser
     {
-        public List<Item> ItemsDoCardapio = JsonParser.ReceberJson();
+        public List<Item> ItemsDoCardapio = JsonParser.ReceberJson(@"C:\Users\luanar\source\repos\MarmitaEletronica\Cardapio\Itens.json");
 
         public List<Item> OrdenarItensPorNomeEID()
         {
@@ -21,7 +21,7 @@ namespace Cardapio.TiposCardapios
 
         public List<Item> OrdenarItensPorTipo()
         {
-            return ItemsDoCardapio.OrderBy(item => item.Tipo).ToList();
+            return ItemsDoCardapio.OrderBy(item => item.Tipo.ToString()).ToList(); // Converted to string for ordering
         }
 
         public List<Item> OrdenarItensPorPreco()
@@ -37,14 +37,24 @@ namespace Cardapio.TiposCardapios
         public void AdicionarItem(Item novoItem) => ItemsDoCardapio.Add(novoItem);
         public void RemoverItem(Item removerItem) => ItemsDoCardapio.Remove(removerItem);
 
-        public void MostrarCardapioOrdenadoPorNome()
+        public void MostrarCardapioOrdenadoPorNome() // Print de todo cardapio
         {
             Console.WriteLine($"--- Cardápio de Refeições Ordenado por Nome ---");
 
             var itensOrdenados = OrdenarItensPorNome();
             foreach (var item in itensOrdenados)
             {
-                Console.WriteLine($"{item.Id} - {item.Nome} - {item.Preco:C2} - {item.Descricao} - Calorias: {item.Calorias}");
+                Console.WriteLine($"Id: {item.Id} - Nome: {item.Nome} - Preço: {item.Preco:C2} - Descrição: {item.Descricao} - Calorias: {item.Calorias}");
+            }
+        }
+
+        public void MostrarCardapioOrdenado(List<Item> items)   // Print de parte específica do cardapio
+        {
+            Console.WriteLine($"--- Cardápio de Refeições Ordenado por Nome ---");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"Id: {item.Id} - Nome: {item.Nome} - Preço: {item.Preco:C2} - Descrição: {item.Descricao} - Calorias: {item.Calorias}");
             }
         }
     }
