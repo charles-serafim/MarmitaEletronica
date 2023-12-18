b8a5374c3532deb1118d0a4bd1af567121191981
Igor de eça almeida <igordrillpsn@gmail.com>
2023-12-17 22:10:54 -0300

    Alterações em recepcionista

--- Funcionarios/TiposFuncionarios/Recepcionista.cs
+++ Funcionarios/TiposFuncionarios/Recepcionista.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
+using GestaoDePedidos.Mesas;
 
 namespace Funcionarios.TiposFuncionarios
 {
-    internal class Recepcionista : Funcionario
+    public class Recepcionista : Funcionario
     {
         public Recepcionista(string nome, int id)
             : base(nome, id)
         {
 
+        }
+        public List<Mesas> Mesas { get; set; }       
+        Mesas = LeitorDeJson();
+
+        public Mesas? SelecionarMesaR(int resposta)
+        {
+            if (Mesas.Find(x => x.Lugares == resposta && x.status == StatusMesa.Livre) != null)
+            {
+                return Mesas.Find(x => x.Lugares == resposta && x.status == StatusMesa.Livre);
+            }
+            else if (Mesas.Find(x => x.Lugares > resposta && x.status == StatusMesa.Livre) != null)
+            {
+                return Mesas.Find(x => x.Lugares > resposta && x.status == StatusMesa.Livre);
+            }
+            else
+            {
+                Console.WriteLine("Não há mesas disponíveis que atendam a quantidade de clientes, por favor aguarde um momento.");
+                return null;
+            }
         }
     }
 }
